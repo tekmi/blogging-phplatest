614ff9f6a95602b719c356049689126c1da30c03
Adam Gegotek <a.gegotek@tekmi.nl>
2017-10-29 16:44:03 +0100

    PHP 7.2 Object Typehint

---
+++ php-7.2/object_typehint/index.php
@@ -0,0 +1,38 @@
+<?php
+
+class Car
+{
+}
+
+class Person
+{
+}
+
+/**
+ * @param object $obj
+ * @return string
+ */
+function moves(object $obj): string
+{
+    return get_class($obj);
+}
+
+/**
+ * @param array $person
+ * @return object
+ */
+function converts(array $person): object {
+    return (object) $person;
+}
+
+var_dump(
+    moves(new Car()),
+    moves(new Person()),
+    moves(json_decode('{}'))
+);
+
+var_dump(converts([
+    "name" => "Adam",
+    "surname" => "Geg",
+    "city" => "Amsterdam"
+]));
